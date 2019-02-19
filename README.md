402d3735ce5e316c18a42fc1ae19c0c63b49f4f9
QBatista <quentin.batista@mail.mcgill.ca>
2019-02-19 03:28:47 -0500

    Add `setup.py` to make the package installable

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 
 ## Knightian Model of Innovation
 
-Scripts to solve the Knightian model of innovation.
+A package to solve the Knightian model of innovation.
+
+### Installation
+
+To install `knightian_model`, first download the source code, for example, by opening a terminal prompt and typing
+
+```
+git clone https://github.com/QBatista/knightian_model
+```
+
+Once you have downloaded the source code on your machine, run
+
+```
+pip install /path/to/package
+```
+
+Alternatively, if you would like any modification that you make to the source code to be applied without having to reinstall the package, use
+
+```
+pip install -e /path/to/package
+```
 
 ### Tests
 
---
+++ setup.py
@@ -0,0 +1,23 @@
+# Use setuptools in preference to distutils
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os
+
+DESCRIPTION = "A package for solving the Knightian Model of Innovation"
+
+setup(name='knightian_model',
+      packages=['knightian_model'],
+      version=0.1,
+      description=DESCRIPTION,
+      author='Quentin Batista',
+      author_email='batista.quent@gmail.com',
+      url='https://github.com/QBatista/knightian_model',  # URL to the repo
+      keywords=['quantitative', 'economics', 'knightian'],
+      install_requires=[
+          'numba>=0.38',
+          'numpy',
+          'interpolation',
+          ]
+      )
